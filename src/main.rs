6dd7eac0be29c9508f2e91385d3ffb5e99d47754
aref-hammaslak <arefhammaslak@gmail.com>
2024-06-30 08:08:55 +0330

    Create a vector of vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
+    let mut rows = vec![vec![1, 2, 3],  vec![4, 5, 6], vec![ 7, 8, 9]];
+    for row  in &rows {
+        for num in row {
+            print!("{} ", num);
+        }
+        println!();
+    }
 
 }
